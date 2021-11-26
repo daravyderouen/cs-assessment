a6e6b6c2d0cde58b3999d39519426c15f1c2699c
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-26 17:32:53 -0600

    Adding in more function codes

--- cs/differentFunctionCodes.js
+++ cs/differentFunctionCodes.js
@@ -6,6 +6,58 @@ function addToZero (x,y) {
     }
 }
 
-let addtoZeroRuntime = "linear time, O(n)"
-//console.log(addToZero(3,2))
-//console.log(addToZero(0,0))
+let addtoZeroRuntime = "linear time, O(n)"/console.log(addToZero(3,2))
+console.log(addToZero(0,0))
+
+
+
+
+
+let isUnique = (str) => {
+    const hash = {};
+    for (const i in str) {
+      if (hash[str[i]]) return false;
+      hash[str[i]] = true;
+    }
+    return true;
+  };
+  
+  console.log(isUnique('Louisiana'));
+  console.log(isUnique('Dad'));
+
+
+
+
+
+
+
+  function isPangram (string) {
+      let strArr = string.toLowerCase();
+      let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+      for (let i = 0; i < alphabet.length; i++) {
+          if(strArr.indexOf(alphabet[i]) < 0) {
+              return false;
+          }
+      }
+
+      return true;
+  }
+
+  console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+
+  console.log(isPangram("Butters is tan."))
+
+
+
+  
+
+
+
+  let randomArray = ["Kitties", "are", "better", "thank", "Goodness"]
+
+  newArr = randomArray.sort(function(a, b) {return b.length - a.length})[0].length;
+  console.log(newArr)
+
+  
+ 
